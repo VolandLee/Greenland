5cbdc3f5f5d3e7400dbe7afdb154cc0aef5274cd
VolandLee <vonkyvonkyvonky@gmail.com>
2022-11-03 13:33:24 +0500

    addnewmodels

--- Greenland/Supp_ord/admin.py
+++ Greenland/Supp_ord/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Catalog)
+admin.site.register(City)
+admin.site.register(Region)
+admin.site.register(Clients)
+admin.site.register(Premise_type)
+admin.site.register(Premises)
+admin.site.register(Employees)
--- Greenland/Supp_ord/models.py
+++ Greenland/Supp_ord/models.py
@@ -25,3 +25,114 @@ class Catalog(models.Model):
     def __str__(self):
         return f'id={self.catalog_id}, {self.catalog_name}, Родитель_{self.parent}'
 
+
+class Premise_type(models.Model):
+    premise_type_id = models.AutoField(primary_key=True)
+    type_decryp = models.CharField(max_length=45)
+
+    class Meta:
+        managed = False
+        db_table = 'premise_type'
+
+    def __str__(self):
+        return f'id={self.premise_type_id}, {self.type_decryp}'
+
+
+class City(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    city_name = models.CharField(max_length=45)
+
+
+    class Meta:
+        managed = False
+        db_table = 'city'
+
+    def __str__(self):
+        return f'id={self.city_id}, {self.city_name}'
+
+
+class Region(models.Model):
+    region_id = models.AutoField(primary_key=True)
+    region_name = models.CharField(max_length=90)
+
+
+    class Meta:
+        managed = False
+        db_table = 'region'
+
+    def __str__(self):
+        return f'id={self.region_id}, {self.region_name}'
+
+class Clients(models.Model):
+    client_id = models.AutoField(primary_key=True)
+    FName = models.CharField(max_length=45)
+    LName = models.CharField(max_length=45)
+    Patronymic = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    phone = models.BigIntegerField
+    postcode = models.IntegerField()
+    region = models.ForeignKey('Region', models.DO_NOTHING, null=True)
+    city = models.ForeignKey('City', models.DO_NOTHING, null=True)
+    street = models.CharField(max_length=45)
+    h_number = models.IntegerField()
+    gender = models.BooleanField()
+    birth_date = models.DateField()
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    liked = models.JSONField(blank=True, null=True)
+
+
+    class Meta:
+        managed = False
+        db_table = 'clients'
+    def __str__(self):
+        return f'id={self.client_id}, {self.FName}, {self.LName}'
+
+
+class Premises(models.Model):
+    premise_id = models.AutoField(primary_key=True)
+    premise_type = models.ForeignKey('Premise_type', models.DO_NOTHING, null=True)
+    coord_long = models.FloatField()
+    coord_lat = models.FloatField()
+    email = models.CharField(max_length=45)
+    phone = models.BigIntegerField
+    postcode = models.IntegerField()
+    region = models.ForeignKey('Region', models.DO_NOTHING, null=True)
+    city = models.ForeignKey('City', models.DO_NOTHING, null=True)
+    street = models.CharField(max_length=45)
+    h_number = models.IntegerField()
+
+
+    class Meta:
+        managed = False
+        db_table = 'premises'
+    def __str__(self):
+        return f'id={self.premise_id}, {self.premise_type_id}, {self.region}, {self.city}'
+
+
+class Employees(models.Model):
+    employee_id = models.AutoField(primary_key=True)
+    premise = models.ForeignKey('Premises', models.DO_NOTHING, null=True)
+    FName = models.CharField(max_length=45)
+    LName = models.CharField(max_length=45)
+    Patronymic = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    phone = models.BigIntegerField
+    postcode = models.IntegerField()
+    region = models.ForeignKey('Region', models.DO_NOTHING, null=True)
+    city = models.ForeignKey('City', models.DO_NOTHING, null=True)
+    street = models.CharField(max_length=45)
+    h_number = models.IntegerField()
+    gender = models.BooleanField()
+    birth_date = models.DateField()
+    hire_date = models.DateField()
+    chief = models.ForeignKey('Catalog', models.DO_NOTHING, blank=True, null=True)
+
+
+    class Meta:
+        managed = False
+        db_table = 'employees'
+    def __str__(self):
+        return f'id={self.employee_id}, {self.FName}, {self.LName}'
+
